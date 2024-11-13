d1f5701184b2d2bcd819b727b647011165fe10a9
Mikhail Afanasev <Miki123451@mail.ru>
2024-11-13 20:27:37 +0300

    added gam

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,20 +2,26 @@
 
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".'); // знакомим пользователя с правилами игры
 const solution = () => {
   let i = 1;
   while (i <= 3) {
-    const wrong = `'yes' is wrong answer ;(. Correct answer was 'no'.
-Let's try again, ...!`;
-    const neMber = Math.random() * 100;
-    const rNumber = Math.round(neMber); // получаем ранд. число
+    const wrong = `Answer 'yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, ...!`; // сообщение при ошибки в ответе
+    const right = 'Correct!'; // сообщение правильный ответ
+    const neMber = Math.random() * 100; // получаем рандомное число
+    const rNumber = Math.round(neMber); // рандомное число округляем
     console.log(`Question:${rNumber}`); // даем число пользователю
     const answer = readlineSync.question('Your answer: '); // получаем ответ от пользователя
     const checkNumber = rNumber % 2 === 0 ? 'yes' : 'no'; // проверяем на четность число
-    const verification = answer === checkNumber ? 1 : wrong; // проверям ответ с числом
+    const verification = answer === checkNumber ? 1 : 0; // проверям ответ с числом
     if (verification === 1) {
-      console.log('Correct!');
+      console.log(right); // при правильном ответе продолжаем игру
+    } else {
+      return wrong; // при не правильном ответе заканчивает игру
     }
     i += 1;
   }
